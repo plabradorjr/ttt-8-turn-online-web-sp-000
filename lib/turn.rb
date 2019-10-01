06baac9b79d0ba7661a2bce34f890a6503ca7bfd
Pancho Labrador <plabradorjr@gmail.com>
2019-10-01 13:33:06 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,26 +6,18 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-
 def input_to_index(user_input)
   user_input.to_i - 1
-end
-
-def position_taken?(board, index)
-  board[index] != " "
 end
 
 def valid_move?(board, index)
   if index.between?(0,8)
     if !position_taken?(board, index)
       true
+    end
   end
 end
 
-
-def move(board, index, current_player = "X")
-  board[index] = current_player
-end
 
 
 def turn(board)
@@ -38,4 +30,12 @@ def turn(board)
   else
     turn(board)
   end
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
